20e7b543b4ff2520e82e78bd154f2bd351623e01
Justyna Ciesla <justyna.ciesla@people-analytix.com>
2023-01-18 15:11:50 +0100

    small update

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routes } from "../routes";
 import MainTemplate from "../templates/MainTemplate";
 import Home from "../views/home";
-import Destination from "../views/destination";
 import Crew from "../views/crew";
 import Technology from "../views/technology";
 import Planet from "../views/planet";
@@ -15,13 +14,7 @@ const Router = () => {
         <Routes>
           <Route exact path={routes.home} element={<Home />} />
           <Route path={routes.planetDestination} element={<Planet />} />
-          {/* <Route path={routes.destination} element={<Destination />} /> */}
-
           <Route path={routes.crewDetails} element={<Crew />} />
-
-          {/* <Route path={routes.crew} element={<Crew />} /> */}
-
-          {/* <Route path={routes.technology} element={<Technology />} /> */}
           <Route path={routes.technologyDetails} element={<Technology />} />
         </Routes>
       </MainTemplate>
